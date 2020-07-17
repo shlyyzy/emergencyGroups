1637661bbf1ac6ce004783337e912f80e2d98a86
MagY <yangm110615@gmail.com>
2020-07-17 10:21:01 -0700

    router

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,6 +2,11 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 
+import Navbar from './components/Navbar';
+import Chat from './components/Chat';
+import Map from './components/Map';
+import Info from './components/Info';
+
 function App() {
   const [message, setMessage] = useState(null);
   const [isFetching, setIsFetching] = useState(false);
@@ -32,10 +37,14 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/"></Route>
-        <Route path="/chat"></Route>
-        <Route path="/map"></Route>
-        <Route path="/info"></Route>
+        <Navbar />
+        <br/>
+        <Switch>
+          <Route exact path="/"></Route>
+          <Route path="/chat"></Route>
+          <Route path="/map"></Route>
+          <Route path="/info"></Route>
+        </Switch>
       </div>
     </Router>
   );
---
+++ react-ui/src/components/Info.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Info extends Component {
+    render() {
+        return (
+            <div>
+                this is the info
+            </div>
+        )
+    }
+}
